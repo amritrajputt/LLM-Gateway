925abe2426d4acb0f3317532bd199a967fef3b42
amrit <vishal179singh@gmail.com>
2026-08-22 23:44:00 +0530

    cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,12 @@
 import express from 'express'
 import { clerkMiddleware } from '@clerk/express'
-
+import {cors} from 'cors';
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+  origin: 'http://localhost:3000',
+}))
 const PORT = 3000
 
 app.use(clerkMiddleware())
