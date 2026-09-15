7a0083c1208aa3518cb08ed028dd7a3be8c533c7
amrit <vishal179singh@gmail.com>
2026-09-16 01:49:53 +0530

    adding route

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { clerkMiddleware } from '@clerk/express';
 import { authRouter } from './src/module/auth/auth.route';
+import { keysRouter } from './src/module/keys/keys.routes';
 import { errorHandler } from './src/common/middleware/error.middleware';
 
 const app = express();
@@ -10,7 +11,7 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(clerkMiddleware());
 
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/')
+app.use('/api/v1/keys', keysRouter);
 // Global Error Handler
 app.use(errorHandler);
 
